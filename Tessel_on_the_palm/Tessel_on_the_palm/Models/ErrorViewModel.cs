cb58014db01a76d4a239ea5c9aab3f1f97622b12
SuHaRIk0 <rijkotaga@gmail.com>
2024-03-14 23:24:12 +0200

    added structure of program

---
+++ Tessel_on_the_palm/Tessel_on_the_palm/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tessel_on_the_palm.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
