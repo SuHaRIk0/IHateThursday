2923a77eda9ece9fe889266cb50e7c87147bf7d7
SuHaRIk0 <rijkotaga@gmail.com>
2024-04-05 18:48:59 +0300

    in fifth april state of programm

--- src/Web/Controllers/ProfileController.cs
+++ src/Web/Controllers/ProfileController.cs
@@ -3,45 +3,132 @@ using Microsoft.AspNetCore.Mvc;
 using Infrastructure.Data;
 using System.Linq;
 using Web.Models;
+using Application.Services;
+using Domain.IService;
 
 namespace Web.Controllers
 {
+    [Route("[controller]/{action=Profile}")]
     public class ProfileController : Controller
     {
-        private readonly TopDbContext _context;
 
-        public ProfileController(TopDbContext context)
+        //[HttpGet]
+        //public IActionResult ShowProfile(int id)
+        //{
+        //    var user = _context.CommonUsers.FirstOrDefault(u => u.Id == id);
+
+        //    if (user == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var model = new ProfileViewModel
+        //    {
+        //        Name = user.Name,
+        //        Tag = user.Tag,
+        //        Description = user.Description,
+        //        GenresReaded = user.GenresReaded
+        //    };
+
+        //    return View(model);
+        //}
+
+        //[HttpGet]
+        //public IActionResult EditProfile(int id)
+        //{
+        //    var user = _context.CommonUsers.FirstOrDefault(u => u.Id == id);
+
+        //    if (user == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var model = new ProfileViewModel
+        //    {
+        //        Id = user.Id,
+        //        Name = user.Name,
+        //        Tag = user.Tag,
+        //        Description = user.Description,
+        //        GenresReaded = user.GenresReaded
+        //    };
+
+        //    return View(model);
+        //}
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult EditProfile(ProfileViewModel model)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return View(model);
+        //    }
+
+        //    var user = _context.CommonUsers.FirstOrDefault(u => u.Id == model.Id);
+
+        //    if (user == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    user.Name = model.Name;
+        //    user.Tag = model.Tag;
+        //    user.Description = model.Description;
+        //    user.GenresReaded = model.GenresReaded;
+
+        //    _context.SaveChanges();
+
+        //    return RedirectToAction(nameof(ShowProfile), new { id = user.Id });
+        //}
+
+        private readonly IProfileService _profileService;
+
+        public ProfileController(IProfileService profileService)
         {
-            _context = context;
+            _profileService = profileService;
         }
 
-        public IActionResult ShowProfile(int id)
+        [HttpGet("{id}")]
+        public async Task<IActionResult> ShowProfile(int id)
         {
-            var user = _context.CommonUsers.FirstOrDefault(u => u.Id == id);
+            return View(await _profileService.ShowByIdAsync(id));
+        }
 
-            if (user == null)
-            {
-                return NotFound();
-            }
+        [HttpPost("{id}")]
+        public async Task<IActionResult> EditProfile(int id, CommonUser updatedUser)
+        {
+            var dummi = await _profileService.EditByIdAsync(id, updatedUser);
 
-            var model = new ProfileViewModel
-            {
-                Name = user.Name,
-                Tag = user.Tag,
-                Description = user.Description,
-                GenresReaded = user.GenresReaded
-            };
-
-            //var model = new ProfileViewModel
-            //{
-            //    Id = 1,
-            //    Name = "Iryna",
-            //    Tag = "IZ",
-            //    Description = "I love a book",
-            //    GenresReaded = "Fiction, Mystery"
-            //};
-
-            return View(model); 
+            return View(await _profileService.ShowByIdAsync(id));
         }
     }
 }
+
+
+//using Domain.IService;
+//using Microsoft.AspNetCore.Mvc;
+
+//namespace Web.Controllers
+//{
+//    [Route("[controller]/{action=GetRecomendations}")]
+//    public class RecomendationsController : Controller
+//    {
+//        private readonly IRecomendationService _recomendationService;
+//        private readonly IBookTransformService _bookTransformService;
+
+//        public RecomendationsController(IRecomendationService recomendationService, IBookTransformService bookTransformService)
+//        {
+//            _recomendationService = recomendationService;
+//            _bookTransformService = bookTransformService;
+//        }
+
+//        // Matches Recomendations/GetRecomendations/2
+//        [HttpGet("{id}")]
+//        public async Task<IActionResult> GetRecomendations(int id)
+//        {
+//            var books = await _recomendationService.GetRecomendationsAsync(id);
+
+//            return View(await _bookTransformService.GetBookDtosAsync(books));
+//        }
+//    }
+//}
