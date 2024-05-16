458115af9d51d504ad1d3520ed436e17733a1ba2
OleksandrVlasiuk <alexvlasyk31@gmail.com>
2024-05-17 00:59:45 +0300

    model

--- src/Web/Controllers/SubscriptionController.cs
+++ src/Web/Controllers/SubscriptionController.cs
@@ -12,24 +12,27 @@ namespace Web.Controllers
     {
         private readonly ISubscriptionTransformService _subscriptionTransformService;
         private readonly ISubscriptionSearchService _subscriptionSearchService;
+        private readonly TopDbContext _context;
 
-
-        public SubscriptionController(ISubscriptionTransformService subscriptionTransformService , ISubscriptionSearchService subscriptionSearchService)
+        public SubscriptionController(TopDbContext context, ISubscriptionTransformService subscriptionTransformService , ISubscriptionSearchService subscriptionSearchService)
         {
             _subscriptionTransformService = subscriptionTransformService;
             _subscriptionSearchService = subscriptionSearchService;
+            _context = context;
         }
 
         [HttpGet("followers/{tag}")]
         public async Task<IActionResult> GetFollowers(string tag, CancellationToken cancellationToken)
         {
-            var followers = await _subscriptionSearchService.GetFollowersByIdAsync(1, cancellationToken);
+            var user = _context.CommonUsers.FirstOrDefault(u => u.Tag.Equals(tag));
+            var followers = await _subscriptionSearchService.GetFollowersByIdAsync(user.Id, cancellationToken);
             return Ok(followers);
         }
         [HttpGet("subscriptions/{tag}")]
         public async Task<IActionResult> GetSubscriptions(string tag, CancellationToken cancellationToken)
         {
-            var subscriptions = await _subscriptionSearchService.GetSubscriptionsByIdAsync(1, cancellationToken);
+            var user = _context.CommonUsers.FirstOrDefault(u => u.Tag.Equals(tag));
+            var subscriptions = await _subscriptionSearchService.GetSubscriptionsByIdAsync(user.Id, cancellationToken);
             return Ok(subscriptions);
         }
     }
---
+++ src/Web/Models/SubInfoViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Web.Models
+{
+    public class SubInfoViewModel
+    {
+        public int id_user;
+        public int subscribtions_amount;
+        public int followers_amount;
+        public List<String> followers;
+        public List<String> subscriptions;
+    }
+}
